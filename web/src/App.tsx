f8c2d667e6d4085a177e934cb84b3be409714f6c
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-09-13 17:36:39 -0300

    Added title + logo and CSS

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,10 @@
 import './styles/main.css'
+import fdLogo from './assets/img/fd__logo.png'
 
 function App() {
-  return <div >
-    
+  return <div className='max-w-[1344px] mx-auto flex flex-col items-center my-10'>
+    <img src={fdLogo} alt="eSports logo" />
+    <h1 className='text-6xl text-white font-black mt-10'>Seu <span className='bg-fd-gradient bg-clip-text text-transparent'>duo</span> está aqui.</h1>
   </div>
 }
 
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        fdBG: 'url("/img/fd__bg.png")'
+        fdBG: 'url("/img/fd__bg.png")',
+        'fd-gradient': 'linear-gradient(90deg, #1e3a8a 10%, #be185d 50.52%, #f59e0b 90%)'
       }
     },
   },
