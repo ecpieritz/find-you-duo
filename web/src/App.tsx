c4ab9a6a6111c1445314862dcd1b880ef2e5bf11
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-09-15 11:33:54 -0300

    Added text of grid

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -13,24 +13,54 @@ function App() {
     <h1 className='text-6xl text-white font-black mt-10'>Seu <span className='bg-fd-gradient bg-clip-text text-transparent'>duo</span> está aqui.</h1>
 
     <div className='grid grid-cols-6 gap-6 mt-16'>
-      <a href="">
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src={fdApex} alt="" />
+        <div className='w-full pt-16 pb-4 px-4 bg-fd-gradient-grid absolute bottom-0 left-0 right-0 '>
+          <strong className='font-bold text-white block'>Apex</strong>
+          <span className='text-zinc-300 text-sm'>4 anúncios</span>
+        </div>
       </a>
-      <a href="">
+
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src={fdDbd} alt="" />
+        <div className='w-full pt-16 pb-4 px-4 bg-fd-gradient-grid absolute bottom-0 left-0 right-0 '>
+          <strong className='font-bold text-white block'>Dead by Daylight</strong>
+          <span className='text-zinc-300 text-sm'>4 anúncios</span>
+        </div>
       </a>
-      <a href="">
+
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src={fdDota} alt="" />
+        <div className='w-full pt-16 pb-4 px-4 bg-fd-gradient-grid absolute bottom-0 left-0 right-0 '>
+          <strong className='font-bold text-white block'>Dota</strong>
+          <span className='text-zinc-300 text-sm'>4 anúncios</span>
+        </div>
       </a>
-      <a href="">
+
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src={fdFortnite} alt="" />
+        <div className='w-full pt-16 pb-4 px-4 bg-fd-gradient-grid absolute bottom-0 left-0 right-0 '>
+          <strong className='font-bold text-white block'>Fortnite</strong>
+          <span className='text-zinc-300 text-sm'>4 anúncios</span>
+        </div>
       </a>
-      <a href="">
+
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src={fdLol} alt="" />
+        <div className='w-full pt-16 pb-4 px-4 bg-fd-gradient-grid absolute bottom-0 left-0 right-0 '>
+          <strong className='font-bold text-white block'>League of Legends</strong>
+          <span className='text-zinc-300 text-sm'>4 anúncios</span>
+        </div>
       </a>
-      <a href="">
+
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src={fdValorant} alt="" />
+        <div className='w-full pt-16 pb-4 px-4 bg-fd-gradient-grid absolute bottom-0 left-0 right-0 '>
+          <strong className='font-bold text-white block'>Valorant</strong>
+          <span className='text-zinc-300 text-sm'>4 anúncios</span>
+        </div>
       </a>
+
     </div>
   </div>
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -8,7 +8,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         fdBG: 'url("/img/fd__bg.png")',
-        'fd-gradient': 'linear-gradient(90deg, #1e3a8a 10%, #be185d 50.52%, #f59e0b 90%)'
+        'fd-gradient': 'linear-gradient(90deg, #1e3a8a 10%, #be185d 50.52%, #f59e0b 90%)',
+        'fd-gradient-grid': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, .9) 67%)',
       }
     },
   },
